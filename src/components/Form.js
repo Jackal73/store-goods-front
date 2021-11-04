3a1055d9d7055658566aaddccd49bf899a1cea18
Shawn Kebel <fxrender@outlook.com>
2021-11-04 12:34:57 -0500

    Add Form.js component

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+export default `<form class="flex flex-col gap-2">
+  <div>
+    <label class="sr-only" for="search">Search</label>
+    <input type="search" placeholder="Search..." id="search" />
+  </div>
+
+  <div>
+    <label for="in-stock-only">Only show storeGoods in stock</label>
+    <input type="checkbox" id="in-stock-only" />
+  </div>
+</form>
+`;
