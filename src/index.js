e0d8d7d5a2df6a2440f57b9c2a68e86584eb69c5
Shawn Kebel <fxrender@outlook.com>
2021-11-04 12:56:48 -0500

    Get storeGoods to input into form list

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import api from "./services/api.service";
 import Form from "./components/Form";
 import List from "./components/List";
 
-// console.log(api.index());
-api.index().then((data) => {
-  console.log(data);
-});
-
 const root = document.querySelector("#root");
 
-root.innerHTML = `<p class="text-center">Hello World!</p>`;
+// console.log(api.index());
+
+api.index().then((storeGoods) => {
+  root.innerHTML = `
+    <main class="container mx-auto flex flex-col items-center">
+      ${Form}
+      ${List(storeGoods)}
+    </main>
+  `;
+
+  root.querySelector("#search").addEventListener("keyup", (e) => {
+    console.log(
+      storeGoods.filter((storeGood) =>
+        storeGood.name.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+    );
+  });
+});
